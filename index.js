7d0570f917b99e9fe422ed7f5e3c0af3a4bff35c
Luis Castillo <mr.luiscastillo@gmail.com>
2023-08-08 23:23:19 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = "mongodb+srv://demouser:demopassword@cluster1.t8zrtt9.mongodb.net/?retryWrites=true&w=majority";
+
+// Connect to MongoDB
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Create a simple schema and model
+const itemSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+app.use(express.json());
+
+// CRUD Routes
+
+// Create an item
+app.post('/items', async (req, res) => {
+  try {
+    const newItem = new Item(req.body);
+    const savedItem = await newItem.save();
+    res.json(savedItem);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Read all items
+app.get('/items', async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Update an item
+app.put('/items/:id', async (req, res) => {
+  try {
+    const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedItem);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Delete an item
+app.delete('/items/:id', async (req, res) => {
+  try {
+    const deletedItem = await Item.findByIdAndRemove(req.params.id);
+    res.json(deletedItem);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
